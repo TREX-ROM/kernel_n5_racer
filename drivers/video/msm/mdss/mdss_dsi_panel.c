bf5211e01c31b574f22d7c2b56ef5b93aee405a3
flar2 <asegaert@gmail.com>
2014-03-16 01:37:53 -0400

    fix unbalanced irq_wake
    Signed-off-by: flar2 <asegaert@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -530,10 +530,6 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 	mutex_unlock(&panel_cmd_mutex);
 
 
-#ifdef CONFIG_PWRKEY_SUSPEND
-	pwrkey_pressed = false;	
-#endif
-		
 	pr_info("%s\n", __func__);
 
 	return 0;
