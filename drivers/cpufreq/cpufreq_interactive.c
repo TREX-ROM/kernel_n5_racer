6f42f00c1f6c66c9552fc8ef154b3b2fbb02c9dc
rachanta <ravikumar.achanta@gmail.com>
2015-11-11 09:04:01 +0530

    cpufreq: interactive: Make window alignment optional
    Make sampling window alignment optional when scheduler inputs
    are not enabled.
    
    Change-Id: If69c111a3efe219cdd1e38c1f46f03404789c0bb
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    
    Conflicts:
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -111,6 +111,13 @@ static int timer_slack_val = DEFAULT_TIMER_SLACK;
 
 
 /*
+ * Whether to align timer windows across all CPUs. When
+ * use_sched_load is true, this flag is ignored and windows
+ * will always be aligned.
+ */
+static bool align_windows = true;
+
+/*
  * Stay at max freq for at least max_freq_hysteresis before dropping
  * frequency.
  */
@@ -716,8 +723,31 @@ static struct global_attr max_freq_hysteresis_attr =
 		store_max_freq_hysteresis);
 
 
-static ssize_t show_min_sample_time(struct kobject *kobj,
-				struct attribute *attr, char *buf)
+
+static ssize_t show_align_windows(struct kobject *kobj,
+				     struct attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", align_windows);
+}
+
+static ssize_t store_align_windows(struct kobject *kobj,
+			struct attribute *attr, const char *buf, size_t count)
+{
+	int ret;
+	unsigned long val;
+
+	ret = strict_strtoul(buf, 0, &val);
+	if (ret < 0)
+		return ret;
+	align_windows = val;
+	return count;
+}
+
+static struct global_attr align_windows_attr = __ATTR(align_windows, 0644,
+		show_align_windows, store_align_windows);
+
+static ssize_t show_go_hispeed_load(struct kobject *kobj,
+				     struct attribute *attr, char *buf)
 {
 	return sprintf(buf, "%lu\n", min_sample_time);
 }
@@ -890,6 +920,7 @@ static struct attribute *interactive_attributes[] = {
 	&boostpulse_duration.attr,
 	&io_is_busy_attr.attr,
 	&max_freq_hysteresis_attr.attr,
+	&align_windows_attr.attr,
 	NULL,
 };
 
