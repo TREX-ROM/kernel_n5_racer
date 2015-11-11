9745e9551a510aa9cd30394bd578e62313a422b0
rachanta <ravikumar.achanta@gmail.com>
2015-11-11 09:06:27 +0530

    cpufreq: interactive: Record hispeed_validated_time in timer function
    Change 0cd554ae585f5ccbb95056e049a8fec0f0742dce
    (cpufreq: interactive: Exercise hispeed settings at a policy level)
    introduced policy level hispeed settings. The change is correct in
    general, but it sets hispeed_validate_time after actual frequency
    change has completed. Waking up speedchange task and setting
    frequency takes non-trival amount of time. This period is not accounted
    for in above_hispeed_delay, resulting in additional delays when
    ramping up frequency. Frequency switch latency varies a lot
    depending on beginning and end frequency, and thus it cannot be easily
    compensated by user setting above_hispeed_delay.
    
    Record a local hispeed_validated_time in every CPU's timer function.
    Cluster hispeed_validated_time is the local hispeed_validated_time of
    CPUs voting for the highest frequency.
    
    Change-Id: Id8ae547fe3a70f8710f60b6e2125954111b7a2b6
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -49,7 +49,9 @@ struct cpufreq_interactive_cpuinfo {
 	unsigned int target_freq;
 	unsigned int floor_freq;
 	u64 floor_validate_time;
-	u64 hispeed_validate_time;
+	u64 hispeed_validate_time; /* cluster hispeed_validate_time */
+	u64 local_hvtime; /* per-cpu hispeed_validate_time */
+	u64 max_freq_idle_start_time;
 	struct rw_semaphore enable_sem;
 	int governor_enabled;
 };
@@ -356,8 +358,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 		goto rearm;
 	}
 
-	pcpu->hispeed_validate_time = now;
-
+	pcpu->local_hvtime = now;
 	if (cpufreq_frequency_table_target(pcpu->policy, pcpu->freq_table,
 					   new_freq, CPUFREQ_RELATION_L,
 					   &index)) {
@@ -512,7 +513,8 @@ static int cpufreq_interactive_speedchange_task(void *data)
 		for_each_cpu(cpu, &tmp_mask) {
 			unsigned int j;
 			unsigned int max_freq = 0;
-
+			struct cpufreq_interactive_cpuinfo *pjcpu;
+			u64 hvt = 0;
 			pcpu = &per_cpu(cpuinfo, cpu);
 			if (!down_read_trylock(&pcpu->enable_sem))
 				continue;
@@ -525,14 +527,24 @@ static int cpufreq_interactive_speedchange_task(void *data)
 				struct cpufreq_interactive_cpuinfo *pjcpu =
 					&per_cpu(cpuinfo, j);
 
-				if (pjcpu->target_freq > max_freq)
+				if (pjcpu->target_freq > max_freq) {
 					max_freq = pjcpu->target_freq;
+					hvt = pjcpu->local_hvtime;
+				} else if (pjcpu->target_freq == max_freq) {
+					hvt = min(hvt, pjcpu->local_hvtime);
+				}
 			}
 
-			if (max_freq != pcpu->policy->cur)
+			if (max_freq != pcpu->policy->cur) {
 				__cpufreq_driver_target(pcpu->policy,
 							max_freq,
 							CPUFREQ_RELATION_H);
+				for_each_cpu(j, pcpu->policy->cpus) {
+					pjcpu = &per_cpu(cpuinfo, j);
+					pjcpu->hispeed_validate_time = hvt;
+				}
+			}
+
 			trace_cpufreq_interactive_setspeed(cpu,
 						     pcpu->target_freq,
 						     pcpu->policy->cur);
@@ -984,6 +996,10 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 				ktime_to_us(ktime_get());
 			pcpu->hispeed_validate_time =
 				pcpu->floor_validate_time;
+
+			pcpu->local_hvtime = pcpu->floor_validate_time;
+			pcpu->max_freq = policy->max;
+
 			down_write(&pcpu->enable_sem);
 			expires = jiffies + usecs_to_jiffies(timer_rate);
 			pcpu->cpu_timer.expires = expires;
