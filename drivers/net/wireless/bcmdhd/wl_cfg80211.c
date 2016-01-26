MotorolaMobilityLLC/kernel-msm

net: wireless: bcmdhd: Fix suspend failure due to wakelock/wakeunlock mismatch
There is an case that shows wake lock remains high number
and prevent device go into sleep.
This is due to not calling unlock as same as lock called.
Fixed lock/unlock sequence to release wake lock correctly.

Bug: 23815756

Change-Id: I0eab6f1ad0d2fa5e65219ee7f4537e9c9371138b
Signed-off-by: Insun Song <isong@broadcom.com>
Signed-off-by: Dmitry Shmidt <dimitrysh@google.com>

drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -10971,7 +10971,8 @@ void wl_cfg80211_detach(struct bcm_cfg80211 *cfg)
 
 static void wl_wakeup_event(struct bcm_cfg80211 *cfg)
 {
-	if (cfg->event_tsk.thr_pid >= 0) {
+	dhd_pub_t *dhd = (dhd_pub_t *)(cfg->pub);
+	if (dhd->up && (cfg->event_tsk.thr_pid >= 0)) {
 		DHD_OS_WAKE_LOCK(cfg->pub);
 		up(&cfg->event_tsk.sema);
 	}
@@ -11030,8 +11031,8 @@ static s32 wl_event_handler(void *data)
 				WL_DBG(("Unknown Event (%d): ignoring\n", e->etype));
 			}
 			wl_put_event(e);
+			DHD_OS_WAKE_UNLOCK(cfg->pub);
 		}
-		DHD_OS_WAKE_UNLOCK(cfg->pub);
 	}
 	WL_ERR(("was terminated\n"));
 	complete_and_exit(&tsk->completed, 0);

TREX-ROM/kernel_n5_racer

Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -9798,7 +9798,8 @@ void wl_cfg80211_detach(void *para)
 
 static void wl_wakeup_event(struct wl_priv *wl)
 {
-	if (wl->event_tsk.thr_pid >= 0) {
+	dhd_pub_t *dhd = (dhd_pub_t *)(wl->pub);
+	if (dhd->up && (wl->event_tsk.thr_pid >= 0)) {
 		DHD_OS_WAKE_LOCK(wl->pub);
 		up(&wl->event_tsk.sema);
 	}