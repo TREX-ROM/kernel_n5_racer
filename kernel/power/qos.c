97d4fca5288e97889d81ed719db8bb0e8fca3455
rachanta <ravikumar.achanta@gmail.com>
2016-02-02 23:06:05 +0530

    PM / QoS: Fix deadlock during PM QoS vote to default value
    pm_qos_work_fn() calls pm_qos_update_request() which does
    cancel_delayed_work_sync() on the same pm_qos_work_fn()
    causing deadlock. Fix this by updating the target with
    default timeout value directly instead of calling
    pm_qos_update_request().
    
    Signed-off-by: Sujit Reddy Thumma <sthumma@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -246,6 +246,7 @@ static void __pm_qos_update_request(struct pm_qos_request *req,
  */
 static void pm_qos_work_fn(struct work_struct *work)
 {
+	s32 new_value = PM_QOS_DEFAULT_VALUE;
 	struct pm_qos_request *req = container_of(to_delayed_work(work),
 						  struct pm_qos_request,
 						  work);
@@ -254,8 +255,10 @@ static void pm_qos_work_fn(struct work_struct *work)
 	if (!req || !pm_qos_request_active(req))
 		return;
 
-	__pm_qos_update_request(req, PM_QOS_DEFAULT_VALUE);
-
+	if (new_value != req->node.prio)
+		pm_qos_update_target(
+			pm_qos_array[req->pm_qos_class]->constraints,
+			&req->node, PM_QOS_UPDATE_REQ, new_value);
 }
 
 /**
