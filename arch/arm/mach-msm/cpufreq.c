30a1335d53386709fb68f5090129535bb28aded1
rachanta <ravikumar.achanta@gmail.com>
2016-02-05 19:39:15 +0530

    qcom-cpufreq: Fix hotplug blocking logic
    Change 2b63b10 qcom-cpufreq: Block hotplug until cpufreq is ready
    intended to block hotplug until qcom-cpufreq driver has got all
    clocks. However, its implementation still leaves a small window when
    CPU 0 has got its clock but others haven't. If a hotplug happens on
    another CPU that doesn't have its clock pointer, refcount of that CPU
    clock will again be wrong.
    
    Fix the issue by introducing a ready flag.
    
    Change-Id: I1e05b063b0584088f717b82709a3f1d55bc6561b
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -262,8 +262,7 @@ static int __cpuinit msm_cpufreq_cpu_callback(struct notifier_block *nfb,
 
 	/* Fail hotplug until this driver can get CPU clocks */
 
-	if (!cpu_clk[0])
-
+	if (!hotplug_ready)
 		return NOTIFY_BAD;
 
 	switch (action & ~CPU_TASKS_FROZEN) {
@@ -482,6 +481,11 @@ static int __init msm_cpufreq_probe(struct platform_device *pdev)
 			return PTR_ERR(c);
 		cpu_clk[cpu] = c;
 	}
+
+
+	if (!cpu_clk[0])
+		return -ENODEV;
+
 	hotplug_ready = true;
 
 	ret = cpufreq_parse_dt(dev);
