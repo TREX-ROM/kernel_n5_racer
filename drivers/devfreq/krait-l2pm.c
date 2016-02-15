594339b6e9e703bce61862e19026cf51a6f9cc03
rachanta <ravikumar.achanta@gmail.com>
2016-02-15 10:31:54 +0530

    devfreq: Make cpubw_hwmon governor reusable and hardware agnostic
    Since various hardware platforms might have different ways of monitoring
    the CPU bandwidth, make the cpubw_hwmon governor hardware agnostic so that
    it can be reused across these hardware platforms.
    
    Change-Id: If7af01537dbf1df66551bea2bc73232d9e9264f7
    Signed-off-by: Saravana Kannan <skannan@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/devfreq/krait-l2pm.c
+++ drivers/devfreq/krait-l2pm.c
@@ -23,9 +23,8 @@
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
 #include <linux/of.h>
-#include <linux/spinlock.h>
-#include "governor_bw_hwmon.h"
-#include "governor_cache_hwmon.h"
+#include "governor_cpubw_hwmon.h"
+
 
 #include <mach/msm-krait-l2-accessors.h>
 
@@ -37,36 +36,44 @@
 #define L2PMINTENSET		0x405
 #define L2PMOVSR		0x406
 #define L2PMOVSSET		0x407
-#define L2PMCCNTR		0x409
+
 #define L2PMnEVCNTCR(n)		(0x420 + n * 0x10)
 #define L2PMnEVCNTR(n)		(0x421 + n * 0x10)
 #define L2PMnEVCNTSR(n)		(0x422 + n * 0x10)
 #define L2PMnEVFILTER(n)	(0x423 + n * 0x10)
 #define L2PMnEVTYPER(n)		(0x424 + n * 0x10)
 
-static DEFINE_SPINLOCK(mon_lock);
+
+#define RD_MON	0
+#define WR_MON	1
+
+static void mon_init(void)
+{
+	/* Set up counters 0/1 to count write/read beats */
+	set_l2_indirect_reg(L2PMRESR(2), 0x8B0B0000);
+	set_l2_indirect_reg(L2PMnEVCNTCR(RD_MON), 0x0);
+	set_l2_indirect_reg(L2PMnEVCNTCR(WR_MON), 0x0);
+	set_l2_indirect_reg(L2PMnEVCNTR(RD_MON), 0xFFFFFFFF);
+	set_l2_indirect_reg(L2PMnEVCNTR(WR_MON), 0xFFFFFFFF);
+	set_l2_indirect_reg(L2PMnEVFILTER(RD_MON), 0xF003F);
+	set_l2_indirect_reg(L2PMnEVFILTER(WR_MON), 0xF003F);
+	set_l2_indirect_reg(L2PMnEVTYPER(RD_MON), 0xA);
+	set_l2_indirect_reg(L2PMnEVTYPER(WR_MON), 0xB);
+}
 
 static void global_mon_enable(bool en)
 {
-	static unsigned int cnt;
 	u32 regval;
-
-	spin_lock(&mon_lock);
-	if (en) {
-		cnt++;
-	} else {
-		if (cnt)
-			cnt--;
-	}
 
 	/* Global counter enable */
 	regval = get_l2_indirect_reg(L2PMCR);
-	if (cnt)
+	if (en)
+
 		regval |= BIT(0);
 	else
 		regval &= ~BIT(0);
 	set_l2_indirect_reg(L2PMCR, regval);
-	spin_unlock(&mon_lock);
+
 }
 
 static void mon_enable(int n)
@@ -103,7 +110,7 @@ static u32 mon_set_limit(int n, u32 count)
 	u32 regval;
 
 	regval = 0xFFFFFFFF - count;
-	set_l2_indirect_reg(n == 31 ? L2PMCCNTR : L2PMnEVCNTR(n), regval);
+	set_l2_indirect_reg(L2PMnEVCNTR(n), regval);
 	pr_debug("EV%d start val: %x\n", n, regval);
 
 	return regval;
@@ -112,8 +119,7 @@ static u32 mon_set_limit(int n, u32 count)
 static long mon_get_count(int n, u32 start_val)
 {
 	u32 overflow, count;
-
-	count = get_l2_indirect_reg(n == 31 ? L2PMCCNTR : L2PMnEVCNTR(n));
+	count = get_l2_indirect_reg(L2PMnEVCNTR(n));
 	overflow = get_l2_indirect_reg(L2PMOVSR);
 
 	pr_debug("EV%d ov: %x, cnt: %x\n", n, overflow, count);
@@ -124,32 +130,10 @@ static long mon_get_count(int n, u32 start_val)
 		return count - start_val;
 }
 
-#define RD_MON	0
-#define WR_MON	1
-#define L2_H_REQ_MON	2
-#define L2_M_REQ_MON	3
-#define L2_CYC_MON	31
-
-/* ********** CPUBW specific code  ********** */
 
 static u32 bytes_per_beat;
 static u32 prev_r_start_val;
 static u32 prev_w_start_val;
-static int bw_irq;
-
-static void mon_bw_init(void)
-{
-	/* Set up counters 0/1 to count write/read beats */
-	set_l2_indirect_reg(L2PMRESR(2), 0x8B0B0000);
-	set_l2_indirect_reg(L2PMnEVCNTCR(RD_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVCNTCR(WR_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVCNTR(RD_MON), 0xFFFFFFFF);
-	set_l2_indirect_reg(L2PMnEVCNTR(WR_MON), 0xFFFFFFFF);
-	set_l2_indirect_reg(L2PMnEVFILTER(RD_MON), 0xF003F);
-	set_l2_indirect_reg(L2PMnEVFILTER(WR_MON), 0xF003F);
-	set_l2_indirect_reg(L2PMnEVTYPER(RD_MON), 0xA);
-	set_l2_indirect_reg(L2PMnEVTYPER(WR_MON), 0xB);
-}
 
 /* Returns MBps of read/writes for the sampling window. */
 static unsigned int beats_to_mbps(long long beats, unsigned int us)
@@ -172,12 +156,13 @@ static unsigned int mbps_to_beats(unsigned long mbps, unsigned int ms,
 	return mbps;
 }
 
-static unsigned long meas_bw_and_set_irq(struct bw_hwmon *hw,
-					 unsigned int tol, unsigned int us)
+
+static unsigned long meas_bw_and_set_irq(struct devfreq *df, unsigned int tol,
+					 unsigned int us)
 {
 	unsigned long r_mbps, w_mbps;
 	u32 r_limit, w_limit;
-	unsigned int sample_ms = hw->df->profile->polling_ms;
+	unsigned int sample_ms = df->profile->polling_ms;
 
 	mon_disable(RD_MON);
 	mon_disable(WR_MON);
@@ -201,34 +186,21 @@ static unsigned long meas_bw_and_set_irq(struct bw_hwmon *hw,
 	return r_mbps + w_mbps;
 }
 
-static irqreturn_t bwmon_intr_handler(int irq, void *dev)
-{
-	if (mon_overflow(RD_MON) || mon_overflow(WR_MON)) {
-		update_bw_hwmon(dev);
-		return IRQ_HANDLED;
-	}
 
-	return IRQ_NONE;
+static bool is_valid_bw_irq(struct devfreq *df)
+{
+	return mon_overflow(RD_MON) || mon_overflow(WR_MON);
 }
 
-static int start_bw_hwmon(struct bw_hwmon *hw, unsigned long mbps)
+static int start_bw_hwmon(struct devfreq *df, unsigned long mbps)
 {
 	u32 limit;
-	int ret;
 
-	ret = request_threaded_irq(bw_irq, NULL, bwmon_intr_handler,
-				  IRQF_ONESHOT | IRQF_SHARED,
-				  "bw_hwmon", hw);
-	if (ret) {
-		pr_err("Unable to register interrupt handler!\n");
-		return ret;
-	}
-
-	mon_bw_init();
+	mon_init();
 	mon_disable(RD_MON);
 	mon_disable(WR_MON);
 
-	limit = mbps_to_beats(mbps, hw->df->profile->polling_ms, 0);
+	limit = mbps_to_beats(mbps, df->profile->polling_ms, 0);
 	limit /= 2;
 	prev_r_start_val = mon_set_limit(RD_MON, limit);
 	prev_w_start_val = mon_set_limit(WR_MON, limit);
@@ -242,171 +214,33 @@ static int start_bw_hwmon(struct bw_hwmon *hw, unsigned long mbps)
 	return 0;
 }
 
-static void stop_bw_hwmon(struct bw_hwmon *hw)
+
+static void stop_bw_hwmon(struct devfreq *df)
 {
-	disable_irq(bw_irq);
-	free_irq(bw_irq, hw);
-	global_mon_enable(false);
+>	global_mon_enable(false);
 	mon_disable(RD_MON);
 	mon_disable(WR_MON);
 	mon_irq_enable(RD_MON, false);
 	mon_irq_enable(WR_MON, false);
 }
 
-static struct devfreq_governor devfreq_gov_cpubw_hwmon = {
-	.name = "cpubw_hwmon",
-};
 
-static struct bw_hwmon cpubw_hwmon = {
+static struct cpubw_hwmon bw_hwmon = {
 	.start_hwmon = &start_bw_hwmon,
 	.stop_hwmon = &stop_bw_hwmon,
+	.is_valid_irq = &is_valid_bw_irq,
 	.meas_bw_and_set_irq = &meas_bw_and_set_irq,
-	.gov = &devfreq_gov_cpubw_hwmon,
 };
-
-/* ********** Cache reqs specific code  ********** */
-
-static u32 prev_req_start_val;
-static int cache_irq;
-
-static void mon_mrps_init(void)
-{
-	/* Cache bank requests */
-	set_l2_indirect_reg(L2PMRESR(0), 0x86000001);
-	set_l2_indirect_reg(L2PMnEVCNTCR(L2_H_REQ_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVCNTR(L2_H_REQ_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVFILTER(L2_H_REQ_MON), 0xF003F);
-	set_l2_indirect_reg(L2PMnEVTYPER(L2_H_REQ_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVCNTCR(L2_M_REQ_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVCNTR(L2_M_REQ_MON), 0x0);
-	set_l2_indirect_reg(L2PMnEVFILTER(L2_M_REQ_MON), 0xF003F);
-	set_l2_indirect_reg(L2PMnEVTYPER(L2_M_REQ_MON), 0x3);
-}
-
-/* Returns million requests/sec for the sampling window. */
-static int count_to_mrps(long long count, unsigned int us)
-{
-	do_div(count, us);
-	count++;
-	return count;
-}
-
-static unsigned int mrps_to_count(unsigned int mrps, unsigned int ms,
-				  unsigned int tolerance)
-{
-	mrps += tolerance;
-	mrps *= ms * USEC_PER_MSEC;
-	return mrps;
-}
-
-static unsigned long meas_mrps_and_set_irq(struct cache_hwmon *hw,
-					unsigned int tol, unsigned int us,
-					struct mrps_stats *mrps)
-{
-	u32 limit;
-	unsigned int sample_ms = hw->df->profile->polling_ms;
-	unsigned long f = hw->df->previous_freq;
-	unsigned long t_mrps, m_mrps, l2_cyc;
-
-	mon_disable(L2_H_REQ_MON);
-	mon_disable(L2_M_REQ_MON);
-	mon_disable(L2_CYC_MON);
-
-	t_mrps = mon_get_count(L2_H_REQ_MON, prev_req_start_val);
-	t_mrps = count_to_mrps(t_mrps, us);
-	m_mrps = mon_get_count(L2_M_REQ_MON, 0);
-	m_mrps = count_to_mrps(m_mrps, us);
-
-	l2_cyc = mon_get_count(L2_CYC_MON, 0);
-
-	limit = mrps_to_count(t_mrps, sample_ms, tol);
-	prev_req_start_val = mon_set_limit(L2_H_REQ_MON, limit);
-	mon_set_limit(L2_M_REQ_MON, 0xFFFFFFFF);
-	mon_set_limit(L2_CYC_MON, 0xFFFFFFFF);
-
-	mon_enable(L2_H_REQ_MON);
-	mon_enable(L2_M_REQ_MON);
-	mon_enable(L2_CYC_MON);
-
-	mrps->mrps[HIGH] = t_mrps - m_mrps;
-	mrps->mrps[MED] = m_mrps;
-	mrps->mrps[LOW] = 0;
-	mrps->busy_percent = mult_frac(l2_cyc, 1000, us) * 100 / f;
-
-	return 0;
-}
-
-static irqreturn_t mon_intr_handler(int irq, void *dev)
-{
-	if (mon_overflow(L2_H_REQ_MON) || mon_overflow(L2_M_REQ_MON)) {
-		update_cache_hwmon(dev);
-		return IRQ_HANDLED;
-	}
-	return IRQ_NONE;
-}
-
-static int start_mrps_hwmon(struct cache_hwmon *hw, struct mrps_stats *mrps)
-{
-	u32 limit;
-	int ret;
-
-	ret = request_threaded_irq(cache_irq, NULL, mon_intr_handler,
-			  IRQF_ONESHOT | IRQF_SHARED,
-			  "cache_hwmon", hw);
-	if (ret) {
-		pr_err("Unable to register interrupt handler!\n");
-		return ret;
-	}
-
-	mon_mrps_init();
-	mon_disable(L2_H_REQ_MON);
-	mon_disable(L2_M_REQ_MON);
-	mon_disable(L2_CYC_MON);
-
-	limit = mrps_to_count(mrps->mrps[HIGH], hw->df->profile->polling_ms, 0);
-	prev_req_start_val = mon_set_limit(L2_H_REQ_MON, limit);
-	mon_set_limit(L2_M_REQ_MON, 0xFFFFFFFF);
-	mon_set_limit(L2_CYC_MON, 0xFFFFFFFF);
-
-	mon_irq_enable(L2_H_REQ_MON, true);
-	mon_irq_enable(L2_M_REQ_MON, true);
-	mon_enable(L2_H_REQ_MON);
-	mon_enable(L2_M_REQ_MON);
-	mon_enable(L2_CYC_MON);
-	global_mon_enable(true);
-
-	return 0;
-}
-
-static void stop_mrps_hwmon(struct cache_hwmon *hw)
-{
-	disable_irq(cache_irq);
-	free_irq(cache_irq, hw);
-	global_mon_enable(false);
-	mon_disable(L2_H_REQ_MON);
-	mon_disable(L2_M_REQ_MON);
-	mon_disable(L2_CYC_MON);
-	mon_irq_enable(L2_H_REQ_MON, false);
-	mon_irq_enable(L2_M_REQ_MON, false);
-}
-
-static struct cache_hwmon mrps_hwmon = {
-	.start_hwmon = &start_mrps_hwmon,
-	.stop_hwmon = &stop_mrps_hwmon,
-	.meas_mrps_and_set_irq = &meas_mrps_and_set_irq,
-};
-
-/*************************************************************************/
 
 static int krait_l2pm_driver_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
-	int ret, ret2;
+	int ret;
 
-	bw_irq = platform_get_irq(pdev, 0);
-	if (bw_irq < 0) {
+	bw_hwmon.irq = platform_get_irq(pdev, 0);
+	if (bw_hwmon.irq < 0) {
 		pr_err("Unable to get IRQ number\n");
-		return bw_irq;
+		return bw_hwmon.irq;
 	}
 
 	ret = of_property_read_u32(dev->of_node, "qcom,bytes-per-beat",
@@ -416,22 +250,11 @@ static int krait_l2pm_driver_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	ret = register_bw_hwmon(dev, &cpubw_hwmon);
-	if (ret)
+	ret = register_cpubw_hwmon(&bw_hwmon);
+	if (ret) {
 		pr_err("CPUBW hwmon registration failed\n");
-
-	cache_irq = bw_irq;
-	mrps_hwmon.of_node = of_parse_phandle(dev->of_node, "qcom,target-dev",
-					      0);
-	if (!mrps_hwmon.of_node)
-		return -EINVAL;
-
-	ret2 = register_cache_hwmon(dev, &mrps_hwmon);
-	if (ret2)
-		pr_err("Cache hwmon registration failed\n");
-
-	if (ret && ret2)
-		return ret2;
+		return ret;
+	}
 
 	return 0;
 }
