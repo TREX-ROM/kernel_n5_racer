fd4df5c8e671112fdee5ce4ebc6b240f08da4033
rachanta <ravikumar.achanta@gmail.com>
2016-02-21 16:15:47 +0530

    mdss: dsi: Fix potential race conditions in mdp busy wait
    Current mdp busy wait logic is prone to race conditions if there are
    multiple threads waiting on MDP DMA transfer to be complete because use
    of complete will only unblock one caller. Fix this by doing complete_all
    instead to unblock all waiters.
    
    Change-Id: I7b568f9d5fe3c68dec1532def2664f946925e770
    Signed-off-by: Adrian Salido-Moreno <adrianm@codeaurora.org>
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>
    Signed-off-by: Bharat Pawar <bpawar@codeaurora.org>
    Signed-off-by: Nirmal Abraham <nabrah@codeaurora.org>

--- drivers/video/msm/mdss/mdss_dsi_host.c
+++ drivers/video/msm/mdss/mdss_dsi_host.c
@@ -1533,7 +1533,8 @@ int mdss_dsi_cmd_mdp_busy(struct mdss_dsi_ctrl_pdata *ctrl)
 {
 	unsigned long flags;
 	int need_wait = 0;
-	int rc = 0;
+	int rc = 1;
+
 	pr_debug("%s: %d start busy=%d pid=%d\n", __func__,
 			ctrl->ndx, ctrl->mdp_busy, current->pid);
 	MDSS_XLOG(ctrl->ndx, ctrl->mdp_busy, current->pid, XLOG_FUNC_ENTRY);
@@ -1555,14 +1556,14 @@ int mdss_dsi_cmd_mdp_busy(struct mdss_dsi_ctrl_pdata *ctrl)
 				pr_err("%s: timeout error\n", __func__);
 				MDSS_XLOG_TOUT_HANDLER("mdp", "dsi0_ctrl",
 				"dsi0_phy", "dsi1_ctrl", "dsi1_phy", "panic");
-			rc = -ETIMEDOUT;
 			}
 		}
 	}
 
 	pr_debug("%s: %d done pid=%d\n", __func__, ctrl->ndx, current->pid);
 	MDSS_XLOG(ctrl->ndx, ctrl->mdp_busy, current->pid, XLOG_FUNC_EXIT);
-	return rc;
+
+	return !rc ? -ETIMEDOUT : 0;
 }
 
 int mdss_dsi_cmdlist_tx(struct mdss_dsi_ctrl_pdata *ctrl,
