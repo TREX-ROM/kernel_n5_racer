e14c9fba72d3f4a6f87beb6e10bc08313f192b16
rachanta <ravikumar.achanta@gmail.com>
2016-03-10 22:37:37 +0530

    sched: fair: remove call for ravg in struct entity

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3329,10 +3329,8 @@ static int move_one_task(struct lb_env *env, int *total_run_moved)
 		 * stats here rather than inside move_task().
 		 */
 		schedstat_inc(env->sd, lb_gained[env->idle]);
+		per_cpu(dbs_boost_load_moved, env->dst_cpu) += pct_task_load(p);
 
-		if (sysctl_sched_ravg_window)
-			*total_run_moved += div64_u64((u64)p->se.ravg.demand *
-					100, (u64)(sysctl_sched_ravg_window));
 		return 1;
 	}
 	return 0;
@@ -3391,9 +3389,7 @@ static int move_tasks(struct lb_env *env, int *total_run_moved)
 		move_task(p, env);
 		pulled++;
 		env->load_move -= load;
-		if (sysctl_sched_ravg_window)
-			*total_run_moved += div64_u64((u64)p->se.ravg.demand *
-					100, (u64)(sysctl_sched_ravg_window));
+		per_cpu(dbs_boost_load_moved, env->dst_cpu) += pct_task_load(p);
 
 #ifdef CONFIG_PREEMPT
 		/*
